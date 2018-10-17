6ac11308c54f2d70bb3903588961b39804a63f5d
coder907 <coder907@example.com>
2018-10-17 17:42:33 +0200

    Code cleanup.

--- src/app/core/core-components.module.ts
+++ src/app/core/core-components.module.ts
@@ -8,7 +8,6 @@ import {
   MatSortModule,
 } from '@angular/material';
 
-import { StaticListComponent } from './components/static-list/static-list.component';
 import { ListComponent } from './components/list/list.component';
 import { PostComponent } from './components/post/post.component';
 import { DblClickOrPressDirective } from '../directives/dblclickorpress.directive';
@@ -24,13 +23,11 @@ import { DblClickOrPressDirective } from '../directives/dblclickorpress.directiv
     MatSortModule,
   ],
   declarations: [
-    StaticListComponent,
     ListComponent,
     PostComponent,
     DblClickOrPressDirective,
   ],
   exports: [
-    StaticListComponent,
     ListComponent,
     PostComponent,
     DblClickOrPressDirective,
