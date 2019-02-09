f8c57290347440b45717c94007fef5ba3eb8123b
coder907 <coder907@example.com>
2019-02-09 18:59:20 +0100

    Some naming fixes.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/my-notes-new'),
+      dir: require('path').join(__dirname, '../coverage/my-notes'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
